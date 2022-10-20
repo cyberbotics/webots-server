4f8f0b2f467cb30c0eb1ef95e69b60e41533585b
Yannick Goumaz <61198661+ygoumaz@users.noreply.github.com>
2022-10-20 15:11:35 +0200

    Update local_simulation_server.py
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>

--- local_simulation_server.py
+++ local_simulation_server.py
@@ -55,7 +55,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print(f"Connected by {addr}")
+        print(f'Connection from {addr}')
         while True:
             data = conn.recv(1024)
             if not data:
