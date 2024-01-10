6f513aadb607c88801f3dba585c59ad9e9bd06f4
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-01-10 13:55:59 +0100

    Fixed version parsing

--- simulation_server.py
+++ simulation_server.py
@@ -268,7 +268,7 @@ class Client:
                 with open(world) as world_file:
                     version = world_file.readline().split()[1]
                 webots_default_image = f'cyberbotics/webots.cloud:{version}-ubuntu'
-                if version[0] == 'R' and (version[1, 5] < 2023 or version == "R2023a"):
+                if version[0] == 'R' and (int(version[1:5]) < 2023 or version == "R2023a"):
                     webots_default_image += '20.04'
                 else:
                     webots_default_image += '22.04'
